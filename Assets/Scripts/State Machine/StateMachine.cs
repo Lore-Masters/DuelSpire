d571f5c7c3bd0a1bdb087f7eca9d5f961623dd80
Skylore Evans <skyloreevanswork@gmail.com>
2024-09-24 21:18:40 -0500

    The Worst State Machine You've Ever Seen :tm:

---
+++ Assets/Scripts/State Machine/StateMachine.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using deVoid.Utils;
+
+public enum State
+{
+	StartOfMatchState,
+	PlayerSetupState,
+	MonsterSetupState,
+	StartOfPlayerTurnState,
+	EndOfPlayerTurnState,
+	StartOfEnemyTurnState,
+	EndOfEnemyTurnState,
+	EndOfCombatState
+};
+
+public class StartOfMatchSignal : ASignal { }
+public class PlayerSetupSignal : ASignal { }
+public class MonsterSetupSignal : ASignal { }
+public class StartOfPlayerTurnSignal : ASignal { }
+public class EndOfPlayerTurnSignal : ASignal { }
+public class StartOfEnemyTurnSignal : ASignal { }
+public class EndOfEnemyTurnSignal : ASignal { }
+public class EndOfCombatSignal : ASignal { }
+
+public class StateMachine : MonoBehaviour
+{
+	public State currentState;
+
+	private void Start()
+	{
+		Signals.Get<StartOfMatchSignal>().AddListener(SetStartOfMatch);
+		Signals.Get<PlayerSetupSignal>().AddListener(SetPlayerSetup);
+		Signals.Get<MonsterSetupSignal>().AddListener(SetMonsterSetup);
+		Signals.Get<StartOfPlayerTurnSignal>().AddListener(SetStartOfPlayerTurn);
+		Signals.Get<EndOfPlayerTurnSignal>().AddListener(SetEndOfPlayerTurn);
+		Signals.Get<StartOfEnemyTurnSignal>().AddListener(SetStartOfEnemyTurn);
+		Signals.Get<EndOfEnemyTurnSignal>().AddListener(SetEndOfEnemyTurn);
+		Signals.Get<EndOfCombatSignal>().AddListener(SetEndOfCombat);
+	}
+
+	private void SetStartOfMatch()
+	{
+		currentState = State.StartOfMatchState;
+	}
+
+	private void SetPlayerSetup()
+	{
+		currentState = State.PlayerSetupState;
+	}
+
+	private void SetMonsterSetup()
+	{
+		currentState = State.MonsterSetupState;
+	}
+
+	private void SetStartOfPlayerTurn()
+	{
+		currentState = State.StartOfPlayerTurnState;
+	}
+
+	private void SetEndOfPlayerTurn()
+	{
+		currentState = State.EndOfPlayerTurnState;
+	}
+
+	private void SetStartOfEnemyTurn()
+	{
+		currentState = State.StartOfEnemyTurnState;
+	}
+
+	private void SetEndOfEnemyTurn()
+	{
+		currentState = State.EndOfEnemyTurnState;
+	}
+
+	private void SetEndOfCombat()
+	{
+		currentState = State.EndOfCombatState;
+	}
+}
---
+++ Assets/Scripts/TestScripts/TestCombat.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using deVoid.Utils;
+
+public class TestCombat : ASignal { }
